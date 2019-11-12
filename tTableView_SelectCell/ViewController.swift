6a92afd2392fcaa3f14cb2ad888addea5c864bc7
tyobigoro <n_nakano@mbm.nifty.com>
2019-11-12 13:23:58 +0900

    Initial Commit

---
+++ tTableView_SelectCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tTableView_SelectCell
+//
+//  Created by tyobigoro_i on 2019/11/12.
+//  Copyright © 2019 tyobigoro_i. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
